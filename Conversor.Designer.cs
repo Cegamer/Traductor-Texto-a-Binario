4b9b5f36c9317061d70baf682d15e100a4a2531d
Cesar Martínez <cesarmauriciomn2012@gmail.com>
2021-07-20 16:38:40 -0500

    Agregar archivos de proyecto.

---
+++ Conversor.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace Traductor_Texto_a_Binario
+{
+    partial class Conversor
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.result = new System.Windows.Forms.Label();
+            this.textInput = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(58, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(174, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Convertir";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // result
+            // 
+            this.result.AutoSize = true;
+            this.result.ForeColor = System.Drawing.Color.Lime;
+            this.result.Location = new System.Drawing.Point(3, 9);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(59, 15);
+            this.result.TabIndex = 1;
+            this.result.Text = "Resultado";
+            this.result.Click += new System.EventHandler(this.result_Click);
+            // 
+            // textInput
+            // 
+            this.textInput.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textInput.Location = new System.Drawing.Point(12, 12);
+            this.textInput.Name = "textInput";
+            this.textInput.Size = new System.Drawing.Size(265, 29);
+            this.textInput.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.result);
+            this.panel1.ImeMode = System.Windows.Forms.ImeMode.Alpha;
+            this.panel1.Location = new System.Drawing.Point(12, 95);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(265, 268);
+            this.panel1.TabIndex = 3;
+            // 
+            // Conversor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(289, 378);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textInput);
+            this.Controls.Add(this.button1);
+            this.MaximumSize = new System.Drawing.Size(305, 417);
+            this.MinimumSize = new System.Drawing.Size(305, 417);
+            this.Name = "Conversor";
+            this.Text = "Conversor";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label result;
+        private System.Windows.Forms.TextBox textInput;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Conversor.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Traductor_Texto_a_Binario
+{
+    public partial class Conversor : Form
+    {
+        public Conversor()
+        {
+            InitializeComponent();
+            result.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            result.Text = CONVERTIR_A_BINARIO(textInput.Text);
+        }
+
+        private string CONVERTIR_A_BINARIO(string Input)
+        {
+            ASCII ascii = new ASCII();
+            string Output = "";
+
+            for (int letraActual = 0; letraActual < Input.Length; letraActual++)
+            {
+                string Letra = Convert.ToString(Input[letraActual]);
+                int codigoAsciiLetra = Array.IndexOf(ascii.Letras_Ascii, Letra);
+
+                for (int Contador2 = 0; Contador2 < ascii.Numero_Bits.Length; Contador2++)
+                {
+                    int[] letraBinario = new int[ascii.Numero_Bits.Length];
+
+                    if (codigoAsciiLetra >= ascii.Numero_Bits[Contador2])
+                    {
+
+                        letraBinario[Contador2] = 1;
+                        codigoAsciiLetra -= ascii.Numero_Bits[Contador2];
+                    }
+
+                    Output = Output + Convert.ToString(letraBinario[Contador2]);
+                }
+
+                if ((letraActual  + 1) % 4 == 0)
+                    Output = Output + "\n";
+
+                else
+                    Output = Output + "   ";
+            }
+
+            return Output;
+        }
+        class ASCII
+        {
+            public int[] Numero_Bits = new int[8];
+            static int[] Ascii_Decimal = new int[255];
+            public string[] Letras_Ascii = new string[255];
+            static string[] Letras_No_Null = { " ", "!", "\"", "#", "$", "%", "&", "\'", "(",")","*","+",",","-",".","/","0","1","2","3","4",
+            "5","6","7","8","9",":",";","<","=",">","?","@","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V" ,
+            "W","X","Y","Z","[","\\","]","^","_","`","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y",
+            "z","{","|","}","~"};
+            public ASCII()
+            {
+                int a = 0;
+                for (int Contador = Numero_Bits.Length - 1; Contador >= 0; Contador--)
+                {
+                    if (Contador == Numero_Bits.Length - 1)
+                    {
+                        Numero_Bits[Contador] = 1;
+                    }
+                    else
+                    {
+                        Numero_Bits[Contador] = Numero_Bits[(Contador + 1)] * 2;
+                    }
+                }
+                for (int Contador = 0; Contador < Ascii_Decimal.Length; Contador++)
+                {
+                    if (Contador < 32 || Contador > 126)
+                    {
+                        Letras_Ascii[Contador] = null;
+                    }
+                    else
+                    {
+                        Letras_Ascii[Contador] = Letras_No_Null[a];
+                        a++;
+                    }
+                }
+            }
+        }
+
+        private void result_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
